345cdc21d689bf826648490441e3ac98bfe00812
MrJons <shad_stick@hotmail.co.uk>
2016-09-15 19:52:13 +0100

    Weather API added

--- src/interface.js
+++ src/interface.js
@@ -30,4 +30,20 @@ $(document).ready(function(){
     $('#temperature').attr('class', thermostat.energyusage());
   }
 
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+  }
+
+  displayWeather('London');
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    displayWeather(city);
+  });
+
 });
