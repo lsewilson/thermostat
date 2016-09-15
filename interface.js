b229d58b6742d6179942ff312379630583cd7115
johnnydee8 <j.dworzynski@me.com>
2016-09-15 15:16:31 +0100

    added weather API

--- interface.js
+++ interface.js
@@ -2,6 +2,17 @@ $( document ).ready(function() {
 
     thermostat = new Thermostat();
 
+    $.getJSON({
+    url: "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b3e7e921e82d662cfe879bb4a818ce7c",
+    method: 'GET',
+
+    success: function( data ) {
+      var tempr = data.main.temp;
+      console.log(tempr);
+    }
+    });
+
+
     function updateTemp(){
       $("#temperature").text( thermostat._temperature );
       $("#temperature").css( "color", thermostat.colour() );
